cdd7df64b720ebd5c962513b13fd0e20dddce4db
YaBoiDemi <demiladeodunayo2009@gmail.com>
2024-10-31 22:06:12 +0000

    New workspace

---
+++ work.js
@@ -0,0 +1,24 @@
+let x,y,z;
+x = 5
+y = 10
+z = x + y
+
+//Creating a variable
+var num1;
+var num2;
+
+//Using my variable
+
+num1 = 10;
+num2 = 202;
+
+let sum = num1 + num2;
+/*
+Cost code
+*/
+
+const bagPrice = 10;
+const bagDiscount = 24;
+let salePrice = bagPrice + bagDiscount;
+
+let person = "Me", carName = "Lambo" , age = 14 , netWorth = "0 💀"
